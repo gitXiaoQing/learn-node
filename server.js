0677c666a90810b5cc7cad3291eb9e8b3986a967
LGQ <gq_li3051@163.com>
2018-11-26 12:04:27 +0800

    read file from disk

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 var http = require('http');
 var url = require('url');
 
-function start (route) {
+function start (route, handle) {
     function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' received');
         
-        route(pathname);
+        request.setEncoding("utf-8");
 
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.write('Hello World');
-        response.end();
+        request.addListener("data", function (postDataChunk) {
+            postData += postDataChunk;
+            ocnsole.log("Received POST data chunk'" + postDataChunk + "'.");
+        });
+
+        request.addListener("end", function () {
+            route(handle, pathname, response, postData);
+        });
     }
+
     http.createServer(onRequest).listen(8888);
     console.log('Server has started');
 }
 
 exports.start = start;
-
